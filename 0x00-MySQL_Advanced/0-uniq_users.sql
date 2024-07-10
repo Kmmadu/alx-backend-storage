331c382082fe89ce4dfd23bfadcb9b2d5a53e2f7
kmmadu <mmadubugwukingsley@gmail.com>
2024-07-10 22:22:23 +0100

    Testing

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,11 +1,7 @@
--- 0-uniq_users.sql
--- Use the database
-USE hbtn_0d_tvshows;
-
--- Create users table with unique email
-CREATE TABLE IF NOT EXISTS users (
+-- Creates a table with unique users.
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
-
